a1d98203cf8e4c0696483226e8762db7ada0abc4
Zhiwei Han <hanzw356255531@icloud.com>
2019-11-14 09:14:23 +0100

    sync

--- benchmark/recsys/models/pgat_recsys.py
+++ benchmark/recsys/models/pgat_recsys.py
@@ -6,20 +6,63 @@ import os.path as osp
 
 from torch_geometric.datasets import MovieLens
 
-from benchmark.recsys.utils import get_folder_path
-from benchmark.recsys.models import PGATNet
-from benchmark.recsys.train_eval import run_with_kg
-from benchmark.recsys.models import PGATNet
+from .pgat import PGATNet
 
 
 class PGATRecSys(object):
     def __init__(self, dataset_args, model_args, train_args):
-        data = MovieLens(**dataset_args).data.to(train_args['device'])
+        self.data = MovieLens(**dataset_args).data.to(train_args['device'])
         model = PGATNet(
-                data.num_nodes[0],
-                data.num_relations[0],
+                self.data.num_nodes[0],
+                self.data.num_relations[0],
                 **model_args).reset_parameters().to(train_args['device'])
         model.load_state_dict(torch.load(train_args))
+
+    def get_top_n_popular_items(self, n=10):
+        """
+        Get the top n movies from self.data.ratings.
+        Remove the duplicates in self.data.ratings and sort it by movie count.
+        After you find the top N popular movies' item id,
+        look over the details information of item in self.data.movies
+
+        :param n: the number of items, int
+        :return: (item_idx, item_url, item_attr), tuple(int, str, dict(str, str))
+        """
+        raise NotImplemented
+
+    def build_user(self, interactions, demographic_info):
+        """
+        Build user profiles given the historical user interactions
+        :param interactions: [N, 2] np.array, [:, 0] is iids and [:, 1] is ratings
+        :param demographic_info: (gender, occupation), tuple
+        :return:
+        """
+        new_user_node_id = self.data.node_emb.shape[0]
+
+        new_user_emb = torch.nn.Embedding(1, args.emb_dim).weight.to(self.device)
+        x = torch.cat([self.data.x.detach(), new_user_emb], dim=0)
+
+        self.opt = torch.optim.Adam(new_user_emb, lr=args.lr, weight_decay=args.weight_decay)
+
+        new_edge_index = create_new_edge_index(
+            self.data, interactions, demographic_info, self.device
+        )
+        new_sec_order_edge_index = create_sec_order_new_edge_index(
+            new_edge_index, self.data.edge_index, self.device
+        )
+
+        for _ in args.epochs:
+            user_repr = self.model(x, new_edge_index, new_sec_order_edge_index)[-1:, :]
+            i_node_ids = np.array([self.data.items.loc[self.data.items['iid'] == iid] for iid in iids])
+            item_repr = x[i_node_ids]
+            loss_t = self.loss_func(torch.sum(user_repr * item_repr, dim=1), ratings)
+
+            self.opt.zero_gradients()
+            loss_t.backward()
+            self.opt.step()
+
+            user_repr = self.model(x, new_edge_index, new_sec_order_edge_index)[-1:, :]
+        self.new_user_repr = user_repr
 
     @staticmethod
     def get_weights_path(dataset_args, model_args, train_args):
--- benchmark/recsys/pgat_recsys.py
+++ benchmark/recsys/pgat_recsys.py
@@ -10,7 +10,88 @@ from benchmark.recsys.models import PGATNet
 from benchmark.recsys.train_eval import run_with_kg
 from benchmark.recsys.models import PGATNet
 
+class KGAT_RS(object):
+    def __int__(self, weights_path):
+        dataset = MovieLens(**dataset_args)
+        self.data = dataset.data
 
+        self.model = PGAT()
+        self.model.restore(weights_path).train(False)
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.data.to(self.device)
+        self.model.to(self.device)
+
+        self.loss_func = torch.nn.MSELoss()
+
+    def get_top_n_popular_items(self, n=10):
+        """
+        Get the top n movies from self.data.ratings.
+        Remove the duplicates in self.data.ratings and sort it by movie count.
+        After you find the top N popular movies' item id,
+        look over the details information of item in self.data.movies
+
+        :param n: the number of items, int
+        :return: (item_idx, item_url, item_attr), tuple(int, str, dict(str, str))
+        """
+        raise NotImplemented
+
+    def build_user(self, interactions, demographic_info):
+        """
+        Build user profiles given the historical user interactions
+        :param interactions: [N, 2] np.array, [:, 0] is iids and [:, 1] is ratings
+        :param demographic_info: (gender, occupation), tuple
+        :return:
+        """
+        new_user_emb = torch.nn.Embedding(1, args.emb_dim).weight.to(self.device)
+        x = torch.cat([self.data.x.detach(), new_user_emb], dim=0)
+
+        self.opt = torch.optim.Adam(new_user_emb, lr=args.lr, weight_decay=args.weight_decay)
+
+        new_edge_index = create_new_edge_index(
+            self.data, interactions, demographic_info, self.device
+        )
+        new_sec_order_edge_index = create_sec_order_new_edge_index(
+            new_edge_index, self.data.edge_index, self.device
+        )
+
+        for _ in args.epochs:
+            user_repr = self.model(x, new_edge_index, new_sec_order_edge_index)[-1:, :]
+            i_node_ids = np.array([self.data.items.loc[self.data.items['iid'] == iid] for iid in iids])
+            item_repr = x[i_node_ids]
+            loss_t = self.loss_func(torch.sum(user_repr * item_repr, dim=1), ratings)
+
+            self.opt.zero_gradients()
+            loss_t.backward()
+            self.opt.step()
+
+            user_repr = self.model(x, new_edge_index, new_sec_order_edge_index)[-1:, :]
+        self.new_user_repr = user_repr
+
+    def get_recommendations(self, n=10):
+        """
+        Give users and get recommendations
+        :param user_idx: int
+        :param n: number of recommendations, int
+        :return: [(item_idx, attr_attention, neighbor_user_attention)], list(tuple(int, dict(int, foloat), dict(int, float)))
+
+        example:
+            [(134, {'director': 0.2, 'actor': 0.8}), {13: 0.1, 15:0.2, ...}]
+        """
+        unseen_movie_iids = get_unseen_movie_iids(self.data.items, self.interactions)
+        unseen_movie_node_ids = self.data.items[self.data.items['iids'] == unseen_movie_iids]['node_id']
+
+        est_ratings = torch.sum(self.new_user_repr * self.data.x[unseen_movie_node_ids], dim=0).numpy()
+        top_n_item
+
+    def update_user(self, interaction):
+        """
+        update user profiles in the RS
+        :param user_iteractions: {item_idx: rating}, dict(int, int)
+
+        :return: None
+        """
+        pass
 
 ########################################### Parse arguments ###########################################
 parser = argparse.ArgumentParser()
