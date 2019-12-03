18b3850319f3f22e23c39df880c77ed133c308d9
Zhiwei Han <hanzw356255531@icloud.com>
2019-12-03 18:16:31 +0100

    sync

--- benchmark/recsys/models/pgat_recsys.py
+++ benchmark/recsys/models/pgat_recsys.py
@@ -7,7 +7,7 @@ import os.path as osp
 
 from torch_geometric.datasets import MovieLens
 
-from .pgat import KGNet
+from .pgat import PGATNetEx
 
 
 class PGATRecSys(object):
@@ -20,13 +20,12 @@ class PGATRecSys(object):
                 self.data.num_nodes[0],
                 self.data.num_relations[0],
                 **model_args)
-        if not dataset_args['debug']:
-            model.load_state_dict(torch.load(train_args))
+        # if not dataset_args['debug']:
+        #     model.load_state_dict(torch.load(train_args))
         self.model = model.to(train_args['device'])
 
     def get_top_n_popular_items(self, n=10):
         """
-        TODO: Zhe
         Get the top n movies from self.data.ratings.
         Remove the duplicates in self.data.ratings and sort it by movie count.
         After you find the top N popular movies' item id,
@@ -36,8 +35,8 @@ class PGATRecSys(object):
         :return: df: popular item dataframe, df
         """
 
-        ratings_df = self.data.ratings[0]
-        ratings_df = ratings_df.drop_duplicates('iid','first',inplace=True)
+        ratings_df = self.data.ratings[0][['iid', 'movie_count']]
+        ratings_df = ratings_df.drop_duplicates()
         ratings_df = ratings_df.sort_index(axis=0,by='movie_count',ascending=False)
 
         return ratings_df[:n]
@@ -50,9 +49,10 @@ class PGATRecSys(object):
         :return:
         """
         # Build edges for new user
+
         new_user_nid = self.model.node_emb.weight.shape[0]
-        new_user_gender_nid = self.data.gender2nid_map[demographic_info['gender']]
-        new_user_occ_nid = self.data.gender2nid_map[demographic_info['occupation']]
+        new_user_gender_nid = self.data.gender_node_id_map[0][demographic_info[0]]
+        new_user_occ_nid = self.data.occupation_node_id_map[0][demographic_info[1]]
         row = [new_user_nid for i in range(len(iids) + 2)]
         col = iids + [new_user_gender_nid, new_user_occ_nid]
         new_edge_index_np = torch.from_numpy(np.array([row, col]))
@@ -63,30 +63,36 @@ class PGATRecSys(object):
         edge_index_np = self.data.edge_index.numpy()
         edge_index_df = pd.DataFrame({'middle': edge_index_np[0, :], 'tail': edge_index_np[1, :]})
         new_sec_order_edge_df = pd.merge(new_edge_index_df, edge_index_df, on='middle')
-        new_sec_order_edge_np = new_sec_order_edge_df.to_numpy().t()
-        new_sec_order_edge = torch.from_numpy(new_sec_order_edge_np)
+        new_sec_order_edge_np = new_sec_order_edge_df.to_numpy()
+        new_sec_order_edge = torch.from_numpy(new_sec_order_edge_np).to(self.train_args['device']).t()
 
         # Get new user embedding by applying message passing
         node_emb = self.model.node_emb.weight
-        new_user_emb = torch.tensor((1, self.model.node_emb.weight.shape[1]))
+        new_user_emb = torch.nn.Embedding(1, self.model.node_emb.weight.shape[1]).weight
         node_emb = torch.cat((node_emb, new_user_emb), dim=0)
-        self.new_user_emb = self.model(node_emb, new_edge_index, new_sec_order_edge)[-1, :]
+        self.node_emb = self.model.forward_(node_emb, new_edge_index, new_sec_order_edge)
+        self.new_user_emb = self.model.forward_(node_emb, new_edge_index, new_sec_order_edge)[-1, :]
         print('user building done...')
 
-    def get_recommendations(self):
+    def get_recommendations(self, seen_iids):
         # Estimate the feedback values and get the recommendation
-        iids = self.get_top_n_popular_items(self.num_recs + 10)
-        rec_iids = self.get_top_n_popular_items(100)
-        rec_iids = [iid for iid in rec_iids if rec_iids not in iids]
-        rec_nids = [self.data.iid2node_map[iid] for iid in rec_iids]
-        rec_item_emb = self.model.node_emb.weight[rec_nids]
-        est_feedback = torch.sum(self.new_user_emb * rec_item_emb, dim=1).reshape(-1).numpy()
-        rec_iid_idx = np.argsort(est_feedback)[self.num_recs]
+        iids = self.get_top_n_popular_items(200).iid
+        rec_iids = [iid for iid in iids if iid not in seen_iids]
+        rec_iids = np.random.choice(rec_iids, 20)
+        rec_nids = self.data.items[0].iloc[rec_iids].nid.values
+        rec_item_emb = self.node_emb[rec_nids]
+        est_feedback = torch.sum(self.new_user_emb * rec_item_emb, dim=1).reshape(-1).cpu().detach().numpy()
+        rec_iid_idx = np.argsort(est_feedback)[:self.num_recs]
         rec_iids = rec_iids[rec_iid_idx]
 
-        df = self.data.items[self.data.items.iid.isin(rec_iids)]
+        df = self.data.items[0][self.data.items[0].iid.isin(rec_iids)]
 
-        return df
+        exp = get_explanation(df)
+
+        return df, exp
+
+    def get_explanation(self, df):
+        pass
 
     # @staticmethod
     # def get_weights_path(dataset_args, model_args, train_args):
