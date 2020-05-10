ec5e65f7b62ddb1bc83a399c300ca3229d01add6
Zhiwei Han <hanzw356255531@icloud.com>
2020-05-11 01:14:03 +0200

    sync

--- benchmark/recsys/models/__init__.py
+++ benchmark/recsys/models/__init__.py
@@ -1,6 +1,6 @@
 from .gcn import GCN
 from .gat import GAT
-from .pagagcn_old import PAGAGCN
+from .pagagcn import PAGAGCN
 
 __all__ = [
     'GCN',
--- benchmark/recsys/models/pagagcn.py
+++ benchmark/recsys/models/pagagcn.py
@@ -1,91 +1,59 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from torch_scatter import scatter_add
-from torch_geometric.utils import add_remaining_self_loops
-import torch_sparse
-import functools
+
+
+class PAPAGCNChannel(torch.nn.Module):
+    def __init__(self, num_steps, num_nodes, emb_dim, hidden_size, repr_dim, if_use_features):
+        super(PAPAGCNChannel, self).__init__()
+        self.num_steps = num_steps
+        self.num_nodes = num_nodes
+        self.if_use_features = if_use_features
+
+        self.gcn_layers = torch.nn.ModuleList()
+        self.gcn_layers.append(GCNConv(emb_dim, hidden_size))
+        for i in range(num_steps - 2):
+            self.gcn_layers.append(GCNConv(hidden_size, hidden_size))
+        self.gcn_layers.append(GCNConv(hidden_size, repr_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for module in self.gcn_layers:
+            module.reset_parameters()
+
+    def forward(self, edge_index_list, x):
+        if len(edge_index_list) != self.num_steps:
+            raise RuntimeError('Number of input adjacency matrices is not equal to step number!')
+
+        for step_idx in range(self.num_steps - 1):
+            x = F.relu(self.gcn_layers[step_idx](x, edge_index_list[step_idx]))
+        x = self.gcn_layers[-1](x, edge_index_list[-1])
+        x = F.normalize(x)
+        return x
 
 
 class PAGAGCN(torch.nn.Module):
-    def __init__(self, num_nodes, emb_dim, hidden_size, repr_dim, dropout):
+    def __init__(self, meta_path_steps, num_nodes, emb_dim, hidden_size, repr_dim, if_use_features, aggr='concat'):
         super(PAGAGCN, self).__init__()
-        self.num_nodes = num_nodes
-        self.dropout = dropout
+        self.meta_path_steps = meta_path_steps
+        self.if_use_features = if_use_features
+        self.aggr = aggr
 
-        self.att_blocks = torch.nn.ModuleList([ConvEncoder(path_length) for path_length in path_lengths])
+        if not if_use_features:
+            self.x = torch.nn.Embedding(num_nodes, emb_dim, max_norm=1)
 
-        self.conv1 = GCNConv(emb_dim, hidden_size)
-        self.conv2 = GCNConv(hidden_size, repr_dim)
+        self.pagcn_channels = torch.nn.ModuleList()
+        for num_steps in meta_path_steps:
+            self.pagcn_channels.append(PAPAGCNChannel(num_steps, num_nodes, emb_dim, hidden_size, repr_dim, if_use_features))
 
-    def reset_parameters(self):
-        for att_block in self.att_blocks:
-            att_block.reset_parameters()
-        self.conv1.reset_parameters()
-        self.conv2.reset_parameters()
-
-    def norm(self, edge_index, num_nodes, edge_weight=None, improved=False,
-             dtype=None):
-        if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
-                                     device=edge_index.device)
-
-        fill_value = 1 if not improved else 2
-        edge_index, edge_weight = add_remaining_self_loops(
-            edge_index, edge_weight, fill_value, num_nodes)
-
-        row, col = edge_index
-        deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
-        deg_inv_sqrt = deg.pow(-0.5)
-        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
-
-        return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
-
-    def path_transform(self, meta_path_edge_indicis, att_block):
-        dense_meta_path_adjs = [
-            torch.sparse.FloatTensor(
-                adj,
-                torch.ones(adj.shape[1], device=adj.device),
-                torch.Size([self.num_nodes, self.num_nodes])
-            ).to_dense().unsqueeze(0)
-            for adj in meta_path_edge_indicis
-        ]
-        meta_path_tensor = torch.cat(dense_meta_path_adjs, dim=0)
-
-        att = att_block(meta_path_tensor.unsqueeze(0))[0][:, :self.num_nodes, :self.num_nodes]
-        dense_attended_adjs = meta_path_tensor * att
-
-        sparse_adapted_adj = torch.eye(self.num_nodes, device=meta_path_edge_indicis[0].device).to_sparse()
-        for i in range(dense_attended_adjs.shape[0]):
-            sparse_adapted_adj = torch.sparse.mm(sparse_adapted_adj, dense_attended_adjs[i, :, :]).to_sparse()
-            edge_index, norm = self.norm(sparse_adapted_adj.indices(), self.num_nodes, edge_weight=sparse_adapted_adj.values())
-            sparse_adapted_adj = torch.sparse.FloatTensor(
-                edge_index,
-                norm,
-                torch.Size([self.num_nodes, self.num_nodes])
-            ).coalesce()
-
-        return sparse_adapted_adj.indices(), sparse_adapted_adj.values()
-
-    def adj_encoder(self, meta_paths):
-        adapt_adjs = [
-            self.path_transform(meta_path, self.att_blocks[meta_path_idx])
-            for meta_path_idx, meta_path in enumerate(meta_paths)
-        ]
-        sparse_mean_adj_mat = functools.reduce(
-            lambda a, b: (torch.cat([a[0], b[0]], dim=1), torch.cat([a[1], b[1]])),
-            adapt_adjs
-        )
-        adapt_edge_index, adp_edge_weight = torch_sparse.coalesce(
-            sparse_mean_adj_mat[0], sparse_mean_adj_mat[1],
-            m=self.num_nodes, n=self.num_nodes,
-            op='max'
-        )
-        return adapt_edge_index, adp_edge_weight
-
-    def forward(self, x, meta_paths):
-        adp_adj_edge_index, adp_adj_edge_weight = self.adj_encoder(meta_paths)
-        x = F.relu(self.conv1(x, adp_adj_edge_index, edge_weight=adp_adj_edge_weight))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.conv2(x, adp_adj_edge_index, edge_weight=adp_adj_edge_weight)
+    def forward(self, meta_path_edge_index_list, x=None):
+        if not self.if_use_features:
+            x = self.x.weight
+        xs = [module(meta_path_edge_index_list[idx], x) for idx, module in enumerate(self.pagcn_channels)]
+        if self.aggr == 'concat':
+            x = torch.cat(xs, dim=1)
+        else:
+            raise NotImplemented('Other aggr methods not implemeted!')
+        x = F.normalize(x)
         return x
